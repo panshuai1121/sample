a80f450ce744ceaa71c43dd5d52cc827d0cbcfa3
panshuai <348539627@qq.com>
2017-10-14 11:53:26 +0000

    finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,9 +6,13 @@
   </head>
   <body>
     @include('layouts._header')
+
     <div class="container">
-      @yield('content')
-       @include('layouts._footer')
+      <div class="col-md-offset-1 col-md-10">
+        @include('shared._messages')
+        @yield('content')
+        @include('layouts._footer')
+      </div>
     </div>
   </body>
 </html>
